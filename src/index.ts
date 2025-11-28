b33fa9b5213e6fcbe7b2bd50a4838b892f78800c
Mihauke <jestemihau@gmail.com>
2025-11-28 12:26:20 +0100

    Organized imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { getCollection } from "./models/collection";
 import { getBlock } from "./models/block";
 import { getBlockGroup } from "./models/block-group";
+import { getCollection } from "./models/collection";
 import * as Media from "./models/media";
 
-export { getCollection, getBlock, getBlockGroup, Media };
+export { getBlock, getBlockGroup, getCollection, Media };
